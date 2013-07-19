a9270421170735a2f39a1b4edaa26d3f76e01e36
LuXiangrong <luxiangrongwave@gmail.com>
2013-07-19 16:29:37 +0800

    add inputstream param

--- src/main/java/com/lxr/torr2mag/Bencode.java
+++ src/main/java/com/lxr/torr2mag/Bencode.java
@@ -1,75 +1,79 @@
 package com.lxr.torr2mag;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import com.lxr.torr2mag.becode.exception.ParseException;
 
 public class Bencode {
 
+	
 	public String parseString(String encodedString) {
-
-		int tokenIndex = encodedString.indexOf(":");
-
-		if (tokenIndex == -1) {
-			throw new ParseException("String format is <length>:<content>, there is no ':' appeared");
-		}
-
-		try {
-			int length = Integer.valueOf(encodedString.substring(0, tokenIndex), 10);
-
-			if (length <= 0) {
-				throw new ParseException("String format is <length>:<content>, length should a decimal integer and greate than 0");
-			}
-
-			int contentLength = encodedString.length() - tokenIndex - 1;
-
-			if (length > contentLength) {
-				throw new ParseException("String format is <length>:<content>, length should less or equal to content length");
-			}
-
-			return encodedString.substring(tokenIndex + 1, tokenIndex + length + 1);
-		} catch (NumberFormatException e) {
-			throw new ParseException("String format is <length>:<content>, length should a decimal integer and greate than 0", e);
-		}
-
+		return parseString(new ByteArrayInputStream(encodedString.getBytes()));
 	}
 
 	public String parseString(InputStream is) {
-		
-		return "";
+		if (!is.markSupported()) {
+			throw new ParseException("Unsupported inputStream");
+		}
+		try {
+			StringBuffer buffer = new StringBuffer();
+			int c = is.read();
+			while (c != -1 && c != ':') {
+				buffer.append((char) c);
+				c = is.read();
+			}
+			int stringLength = Integer.parseInt(buffer.toString(), 10);
+
+			if (stringLength <= 0) {
+				throw new ParseException("Invalid string format");
+			}
+
+			byte[] byteArray = new byte[stringLength];
+			int realLength = is.read(byteArray);
+
+			if (realLength < stringLength) {
+				throw new ParseException("Invalid string format");
+			}
+
+			return new String(byteArray);
+
+		} catch (IOException | NumberFormatException e) {
+			throw new ParseException("Invalid string format", e);
+		}
 	}
 
 	public int parseInt(String string) {
-
-		if (!string.startsWith("i")) {
-			throw new ParseException("Int format is i<content>e, the prefix should be 'i'");
-		}
-
-		StringBuffer buffer = new StringBuffer();
-		for (int i = 1; i < string.length(); i++) {
-			char c = string.charAt(i);
-			if (c == 'e') {
-				String integerAsString = buffer.toString();
-
-				if (integerAsString.startsWith("-0")) {
-					throw new ParseException("Int format is i<content>e, the content should not start with -0");
-				}
-				if (integerAsString.startsWith("0") && integerAsString.length() > 1) {
-					throw new ParseException("Int format is i<content>e, the content should not start with 0 unless is only 0");
-				}
-
-				try {
-					return Integer.valueOf(integerAsString, 10);
-				} catch (NumberFormatException e) {
-					throw new ParseException("Int format is i<content>e, the content is not a decimal format", e);
-				}
-
-			} else {
-				buffer.append(c);
-			}
-		}
-
-		throw new ParseException("Int format is i<content>e, the suffix should be 'e'");
+		return parseInt(new ByteArrayInputStream(string.getBytes()));
 	}
 
+	public int parseInt(InputStream is) {
+		if (!is.markSupported()) {
+			throw new ParseException("Unsupported inputStream");
+		}
+
+		try {
+			is.reset();
+			StringBuffer buffer = new StringBuffer();
+			int c = is.read();
+			if (c != 'i') {
+				throw new ParseException("Invalid int format");
+			}
+			c = is.read();
+			while (c != -1 && c != 'e') {
+				buffer.append((char) c);
+				c = is.read();
+			}
+
+			String intAsString = buffer.toString();
+			if (intAsString.matches("(^0.+)|(^-0.*)")) {
+				throw new ParseException("Invalid int format");
+			}
+
+			return Integer.parseInt(buffer.toString());
+		} catch (IOException | NumberFormatException e) {
+			throw new ParseException("Invalid int format", e);
+		}
+	}
 }
--- src/test/java/com/lxr/torr2mag/BencodeTest.java
+++ src/test/java/com/lxr/torr2mag/BencodeTest.java
@@ -1,6 +1,9 @@
 package com.lxr.torr2mag;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -22,111 +25,119 @@ public class BencodeTest {
 	}
 
 	@Test
-	public void testGetStringSuccess() {
+	public void testparseStringSuccess() throws FileNotFoundException {
 		String actual = bencode.parseString("4:info");
 		assertEquals("info", actual);
 
 		actual = bencode.parseString("6:stringbb");
 		assertEquals("string", actual);
+
+		actual = bencode.parseString(new ByteArrayInputStream("6:string"
+				.getBytes()));
+		assertEquals("string", actual);
 	}
 
 	@Test
-	public void testGetStringThrowExceptionWhenNoColon() {
+	public void testparseStringThrowExceptionWhenNoColon() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("String format is <length>:<content>, there is no ':' appeared");
+		expectedEx.expectMessage("Invalid string format");
 		bencode.parseString("string");
 	}
 
 	@Test
-	public void testGetStringThrowExceptionWhenLengthIsNotADecimalInteger() {
+	public void testparseStringThrowExceptionWhenLengthIsNotADecimalInteger() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("String format is <length>:<content>, length should a decimal integer and greate than 0");
+		expectedEx.expectMessage("Invalid string format");
 		bencode.parseString("0x12:string");
 	}
 
 	@Test
-	public void testGetStringThrowExceptionWhenLengthLessThanZero() {
+	public void testparseStringThrowExceptionWhenLengthLessThanZero() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("String format is <length>:<content>, length should a decimal integer and greate than 0");
+		expectedEx.expectMessage("Invalid string format");
 		bencode.parseString("-1:string");
 	}
 
 	@Test
-	public void testGetStringThrowExceptionWhenLengthEqualZero() {
+	public void testparseStringThrowExceptionWhenLengthEqualZero() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("String format is <length>:<content>, length should a decimal integer and greate than 0");
+		expectedEx.expectMessage("Invalid string format");
 		bencode.parseString("0:string");
 	}
 
 	@Test
-	public void testGetStringThrowExceptionWhenLengthGrateThanContentStringLength() {
+	public void testparseStringThrowExceptionWhenLengthGrateThanContentStringLength() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("String format is <length>:<content>, length should less or equal to content length");
+		expectedEx.expectMessage("Invalid string format");
 		bencode.parseString("7:string");
 	}
-	
+
 	@Test
-	public void testGetInt() {
+	public void testParseInt() {
 		int actual = bencode.parseInt("i123e");
 		assertEquals(123, actual);
-		
+
 		actual = bencode.parseInt("i0e");
 		assertEquals(0, actual);
-		
+
 		actual = bencode.parseInt("i-123e");
 		assertEquals(-123, actual);
-		
+
 		actual = bencode.parseInt("i123e23");
 		assertEquals(123, actual);
 	}
-	
+
 	@Test
-	public void testGetIntThrowExceptionWhenPrefixIsNotValid() {
+	public void testParseIntThrowExceptionWhenPrefixIsNotValid() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("Int format is i<content>e, the prefix should be 'i'");
+		expectedEx.expectMessage("Invalid int format");
 		bencode.parseInt("I123e");
 	}
-	
+
 	@Test
-	public void testGetIntThrowExceptionWhenSuffixIsNotValid() {
+	public void testParseIntThrowExceptionWhenSuffixIsNotValid() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("Int format is i<content>e, the suffix should be 'e'");
+		expectedEx.expectMessage("Invalid int format");
 		bencode.parseInt("i123aa");
 	}
-	
+
 	@Test
-	public void testGetIntThrowExceptionWhenContentIsNotZeroButStartWithZero() {
+	public void testParseIntThrowExceptionWhenContentIsNotZeroButStartWithZero() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("Int format is i<content>e, the content should not start with 0 unless is only 0");
+		expectedEx.expectMessage("Invalid int format");
 		bencode.parseInt("i0123e");
 	}
-	
+
 	@Test
-	public void testGetIntThrowExceptionWhenContentIsNegativeZero() {
+	public void testParseIntThrowExceptionWhenContentIsNegativeZero() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("Int format is i<content>e, the content should not start with -0");
+		expectedEx.expectMessage("Invalid int format");
 		bencode.parseInt("i-0e");
 	}
-	
+
 	@Test
-	public void testGetIntThrowExceptionWhenContentIsStartWithNegativeZero() {
+	public void testParseIntThrowExceptionWhenContentIsStartWithNegativeZero() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("Int format is i<content>e, the content should not start with -0");
+		expectedEx.expectMessage("Invalid int format");
 		bencode.parseInt("i-0123e");
 	}
-	
+
 	@Test
-	public void testGetIntThrowExceptionWhenContentIsNotDecimalFormat() {
+	public void testParseIntThrowExceptionWhenContentIsNotDecimalFormat() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("Int format is i<content>e, the content is not a decimal format");
+		expectedEx.expectMessage("Invalid int format");
 		bencode.parseInt("i123AAe");
 	}
-	
+
 	@Test
-	public void testGetIntThrowExceptionWhenContentIsBlank() {
+	public void testParseIntThrowExceptionWhenContentIsBlank() {
 		expectedEx.expect(ParseException.class);
-		expectedEx.expectMessage("Int format is i<content>e, the content is not a decimal format");
+		expectedEx.expectMessage("Invalid int format");
 		bencode.parseInt("ie");
+	}
+
+	@Test
+	public void testParseListSuccess() {
 	}
 
 }
